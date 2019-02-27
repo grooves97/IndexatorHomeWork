72cdcd5782c98d74668e43151bde02ad95dcaa8e
Aseka <grooves97@gmail.com>
2019-02-27 19:13:51 +0600

    Easy Peasy HM

---
+++ IndexatorApp/IndexArr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorApp
+{
+    public class IndexArr
+    {
+        private int[] arr;
+        public static int lenght = 5;
+
+        public IndexArr()
+        {
+            arr = new int[lenght];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+
+            set
+            {
+                arr[index] = value * value;
+            }
+        }
+
+    }
+}
---
+++ IndexatorApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter integer range");
+            IndexArr.lenght = int.Parse(Console.ReadLine());
+
+            IndexArr iArr = new IndexArr();
+            Random rand = new Random();
+
+            for(int i=0;i<IndexArr.lenght;i++)
+            {
+                iArr[i] = rand.Next(10);
+                
+            }
+            for (int i = 0; i < IndexArr.lenght; i++)
+            {
+                Console.WriteLine($"{iArr[i]}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
